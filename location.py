6e4354c78db8e9c0df11b6c4122ff66dfc040201
ngxdjt <tyson.huynh@laetottenham.ac.uk>
2025-01-15 10:52:20 +0000

    halved buckshot rewards

--- location.py
+++ location.py
@@ -237,7 +237,7 @@ class BoxPush(Minigame):
     
 class Buckshot(Minigame):
     def __init__(self):
-        super().__init__((500, 50))
+        super().__init__((250, 25))
 
     def play(self, player):
         os.system("clear")
